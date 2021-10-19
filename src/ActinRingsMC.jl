cc888d5559d10e56bd94df593c067bebef52e259
Alexander <alexandercumberworth@gmail.com>
2021-10-19 17:36:25 +0200

    Finish first draft such that it runs
    It doesn't crash and appears reasonable, but needs to be validated and
    tested properly.

--- src/ActinRingsMC.jl
+++ src/ActinRingsMC.jl
@@ -22,14 +22,15 @@ struct SystemParams
     EI::Float64
     Lf::Float64
     lf::Int
-    Nfil::Float64
-    Nsca::Float64
+    Nfil::Int
+    Nsca::Int
 end
 
 struct SimulationParams
     steps::Int
     write_interval::Int
-    ring_radius_move_freq::Float64
+    radius_move_freq::Float64
+    op_filename::String
 end
 
 mutable struct Filament
@@ -40,12 +41,15 @@ mutable struct Filament
     index::Int
 end
 
-# make ops as fields?
-# fix filament length
 mutable struct System
-    filaments::Vector{Filament}
-    ring_r::Float64
     parms::SystemParams
+    filaments::Vector{Filament}
+    radius::Float64
+    energy::Float64
+end
+
+function System(parms::SystemParams, filaments::Vector{Filament}, radius::Float64)
+    System(parms, filaments, radius, 0)
 end
 
 mutable struct Lattice
@@ -57,7 +61,19 @@ end
 
 function Lattice(height::Int)
     occupancy::Dict{Vector{Int},Tuple{Int,Int}} = Dict()
-    Lattice(occupancy, occupancy, deepcopy(occupancy), height)
+    Lattice(occupancy, occupancy, copy(occupancy), height)
+end
+
+function calc_lf(Lf::Float64, delta::Float64)::Int
+    div(Lf, delta) + 1
+end
+
+function calc_Lf(lf::Int, delta::Float64)
+    delta * (lf - 1)
+end
+
+function calc_lattice_height(Nsca::Int, lf::Int)
+    Nsca * lf - Nsca - 1
 end
 
 function use_current_coors!(system::System, lattice::Lattice)
@@ -87,7 +103,7 @@ end
 
 function update_radius(system::System, lattice::Lattice, height::Int)
     lattice.height = height
-    system.ring_r = system.parms.delta*(height - 1)
+    system.radius = system.parms.delta * (height - 1)
     return nothing
 end
 
@@ -101,12 +117,12 @@ function generate_starting_config!(lattice::Lattice, Nfil::Int, Nsca::Int, lf::I
             coors = zeros(2, lf)
             for j in 1:lf
                 coors[:, j] = pos
-                lattice.current_occupancy[deepcopy(pos)] = (Ni, j)
-                lattice.trial_occupancy[deepcopy(pos)] = (Ni, j)
+                lattice.current_occupancy[copy(pos)] = (Ni, j)
+                lattice.trial_occupancy[copy(pos)] = (Ni, j)
                 pos[2] += 1
                 wrap_pos!(lattice, pos)
             end
-            filament = Filament(lf, coors, coors, deepcopy(coors), Ni)
+            filament = Filament(lf, coors, coors, copy(coors), Ni)
             push!(filaments, filament)
             pos[2] += lf - 2
             wrap_pos!(lattice, pos)
@@ -131,7 +147,7 @@ function generate_random_vector()
 end
 
 function calc_filament_bending_energy(system::System)
-    system.parms.EI * system.parms.Lf / (2 * system.ring_r^2)
+    system.parms.EI * system.parms.Lf / (2 * system.radius^2)
 end
 
 function calc_overlap_energy(system::System, l::Int)
@@ -144,7 +160,7 @@ function calc_overlap_energy(system::System, l::Int)
     factor_t * log_t
 end
 
-function calc_filament_overlap_energy(system::System, lattice::Lattice, filament::Filament)
+function calc_overlap_energy(system::System, lattice::Lattice, filament::Filament)
     l = 0
     for i in 1:system.parms.lf
         pos = filament.coors[:, i]
@@ -161,25 +177,25 @@ end
 function calc_total_energy(system::System, lattice::Lattice)
     ene = 0
     for filament in system.filaments
-        ene += calc_filament_overlap_energy(system, lattice, filament)/2
+        ene += calc_overlap_energy(system, lattice, filament)/2
         ene += calc_filament_bending_energy(system)
     end
     return ene
 end
 
-function calc_filament_energy_diff!(system::System, lattice::Lattice, filament::Filament)
+function calc_energy_diff!(system::System, lattice::Lattice, filament::Filament)
     use_current_coors!(system, lattice)
-    cur_overlap_ene = calc_filament_overlap_energy(system, lattice, filament)
+    cur_overlap_ene = calc_overlap_energy(system, lattice, filament)
     cur_bending_ene = calc_filament_bending_energy(system)
 
     use_trial_coors!(system, lattice)
-    trial_overlap_ene = calc_filament_overlap_energy(system, lattice, filament)
+    trial_overlap_ene = calc_overlap_energy(system, lattice, filament)
     trial_bending_ene = calc_filament_bending_energy(system)
 
     trial_overlap_ene + trial_bending_ene - cur_overlap_ene - cur_bending_ene
 end
 
-function calc_system_energy_diff!(system::System, lattice::Lattice)
+function calc_energy_diff!(system::System, lattice::Lattice)
     use_current_coors!(system, lattice)
     current_ene = calc_total_energy(system, lattice)
 
@@ -190,8 +206,7 @@ function calc_system_energy_diff!(system::System, lattice::Lattice)
 end
 
 function ring_and_system_connected(system::System, lattice::Lattice, filament::Filament)
-    filaments_in_path = [filament.index]
-    pos = deepcopy(filament.coors[:, 1])
+    pos = copy(filament.coors[:, 1])
     site_i = 1
     path_length = 0
     ring_contig = false
@@ -201,10 +216,13 @@ function ring_and_system_connected(system::System, lattice::Lattice, filament::F
             adj_pos = [pos[1] + dx, pos[2]]
             if adj_pos in keys(lattice.occupancy)
                 adj_filament_i, adj_site_i = lattice.occupancy[adj_pos]
+                if adj_filament_i in connected_filaments
+                    continue
+                end
                 union!(connected_filaments, adj_filament_i)
                 adj_filament = system.filaments[adj_filament_i]
                 ring_contig, connected_filaments = search_filament_for_path(
-                    system, lattice, adj_filament, adj_site_i, filaments_in_path,
+                    system, lattice, adj_filament, adj_site_i, filament,
                     path_length, ring_contig, connected_filaments)
                 if ring_contig && length(connected_filaments) == system.parms.Nfil
                     return true
@@ -221,12 +239,12 @@ end
 
 # this seems to work but its really long and has too many arguments
 function search_filament_for_path(system::System, lattice::Lattice,
-        filament::Filament, site_i::Int, filaments_in_path::Vector{Int},
+        filament::Filament, site_i::Int, root_filament::Filament,
         path_length::Int, ring_contig::Bool, connected_filaments::Set{Int})
 
-    initial_pos = deepcopy(filament.coors[:, site_i])
+    initial_pos = copy(filament.coors[:, site_i])
     initial_path_length = path_length
-    pos = deepcopy(initial_pos)
+    pos = copy(initial_pos)
     dir = -1
     initial_site_i = site_i
     while true
@@ -247,23 +265,22 @@ function search_filament_for_path(system::System, lattice::Lattice,
             if adj_pos in keys(lattice.occupancy)
                 next_filament_i, next_site_i = lattice.occupancy[adj_pos]
                 union!(connected_filaments, next_filament_i)
-                if (next_filament_i == filaments_in_path[1] &&
+                if (next_filament_i == root_filament.index &&
                     abs(path_length) > filament.lf)
-
                     ring_contig = true
                 end
                 if ring_contig && length(connected_filaments) == system.parms.Nfil
                     return (ring_contig, connected_filaments) 
-                elseif !(next_filament_i in filaments_in_path)
-                    push!(filaments_in_path, filament.index)
-                    next_filament = system.filaments[next_filament_i]
-                    ring_contig, connected_filaments = search_filament_for_path(
-                        system, lattice, next_filament, next_site_i,
-                        filaments_in_path, path_length, ring_contig, connected_filaments)
-                    if ring_contig && length(connected_filaments) == system.parms.Nfil
-                        return (ring_contig, connected_filaments)
-                    end
-                    pop!(filaments_in_path)
+                end
+                if next_filament_i in connected_filaments
+                    continue
+                end
+                next_filament = system.filaments[next_filament_i]
+                ring_contig, connected_filaments = search_filament_for_path(
+                    system, lattice, next_filament, next_site_i,
+                    root_filament, path_length, ring_contig, connected_filaments)
+                if ring_contig && length(connected_filaments) == system.parms.Nfil
+                    return (ring_contig, connected_filaments)
                 end
             end
         end
@@ -275,117 +292,7 @@ function search_filament_for_path(system::System, lattice::Lattice,
     return (ring_contig, connected_filaments)
 end
 
-function translate_filament!(filament::Filament, lattice::Lattice, move_vector::Vector{Int})
-    for i in 1:filament.lf
-        pos = filament.coors[:, i]
-        delete!(lattice.occupancy, pos)
-        pos += move_vector
-        wrap_pos!(lattice, pos)
-        lattice.occupancy[deepcopy(pos)] = (filament.index, i)
-    end
-end
-
-function filament_accept_trial!(filament::Filament, lattice::Lattice)
-    filament.current_coors = deepcopy(filament.trial_coors)
-    lattice.current_occupancy = deepcopy(lattice.trial_occupancy)
-end
-
-function filament_accept_current!(filament::Filament, lattice::Lattice)
-    filament.trial_coors = deepcopy(filament.current_coors)
-    lattice.trial_occupancy = deepcopy(lattice.current_occupancy)
-end
-
-function system_accept_trial!(system::System, lattice::Lattice)
-    for filament in system.filaments
-        filament.current_coors = deepcopy(filament.trial_coors)
-    end
-    lattice.current_occupancy = deepcopy(lattice.trial_occupancy)
-end
-
-function system_accept_current!(system::System, lattice::Lattice)
-    for filament in system.filaments
-        filament.trial_coors = deepcopy(filament.current_coors)
-    end
-    lattice.trial_occupancy = deepcopy(lattice.current_occupancy)
-end
-
-function select_move(simparms::SimulationParams)
-    if simparms.ring_radius_move_freq > rand(Float64)
-        return attempt_ring_radius_move!
-    else
-        return attempt_translation_move!
-    end
-end
-
-function accept_move(delta_energy::Float64)
-    p_accept = min(1, exp(delta_energy))
-    accept = false
-    if p_accept == 1 || p_accept > rand(Float64)
-        accept = true;
-    end
-
-    return accept;
-end
-
-function attempt_translation_move!(system::System, lattice::Lattice)
-    use_trial_coors!(system, lattice)
-    filament = select_rand_filament(system)
-    move_vector = generate_random_vector()
-    translate_filament!(filament, lattice, move_vector)
-    # Should I be accepting the current or just propose again?
-    if !ring_and_system_connected(system, lattice, filament)
-        filament_accept_current!(filament, lattice)
-        return nothing
-    end
-    delta_energy = calc_filament_energy_diff!(system, lattice, filament)
-    if accept_move(delta_energy)
-        filament_accept_trial!(filament, lattice)
-    else
-        filament_accept_current!(filament, lattice)
-    end
-
-    return nothing
-end
-
-function find_filaments_on_boundary(system::System, lattice::Lattice)
-    filaments_on_boundary = []
-    for filament in system.filaments
-        for i in 1:filament.lf
-            if filament.coors[2, i] == lattice.height
-                push!(filaments_on_boundary, (filament, i))
-                break
-            end
-        end
-    end
-    return filaments_on_boundary
-end
-
-function translate_filaments_on_boundary!(filaments_on_boundary, lattice, dir)
-
-    # Remove lattice occupancy
-    for (filament, i) in filaments_on_boundary
-        for j in 1:i
-            pos = filament.coors[:, j]
-            delete!(lattice.occupancy, pos)
-        end
-    end
-
-    # Shift segments of filaments below boundary only
-    for (filament, i) in filaments_on_boundary
-        for j in 1:i
-            filament.coors[2, j] += dir
-            pos = filament.coors[:, j]
-            if !(pos in keys(lattice.occupancy))
-                lattice.occupancy[pos] = (filament.index, j)
-            else
-                return false
-            end
-        end
-    end
-    return true
-end
-
-function check_filaments_contiguous(system::System, lattice::Lattice)
+function filaments_contiguous(system::System, lattice::Lattice)
     for filament in system.filaments
         prev_pos = filament.coors[:, 1]
         for i in 2:filament.lf
@@ -401,56 +308,200 @@ function check_filaments_contiguous(system::System, lattice::Lattice)
     return true
 end
 
-function attempt_ring_radius_move!(system::System, lattice::Lattice)
-    dir = rand([-1, 1])
-    # should I check if at max or min radius here?
+function accept_trial!(filament::Filament, lattice::Lattice)
+    filament.current_coors = copy(filament.trial_coors)
+    lattice.current_occupancy = copy(lattice.trial_occupancy)
+end
+
+function accept_trial!(system::System, lattice::Lattice)
+    for filament in system.filaments
+        filament.current_coors = copy(filament.trial_coors)
+    end
+    lattice.current_occupancy = copy(lattice.trial_occupancy)
+end
+
+function accept_current!(filament::Filament, lattice::Lattice)
+    filament.trial_coors = copy(filament.current_coors)
+    lattice.trial_occupancy = copy(lattice.current_occupancy)
+end
+
+function accept_current!(system::System, lattice::Lattice)
+    for filament in system.filaments
+        filament.trial_coors = copy(filament.current_coors)
+    end
+    lattice.trial_occupancy = copy(lattice.current_occupancy)
+end
+
+function accept_move(delta_energy::Float64)
+    p_accept = min(1, exp(delta_energy))
+    accept = false
+    if p_accept == 1 || p_accept > rand(Float64)
+        accept = true;
+    end
+
+    return accept;
+end
+
+function translate_filament!(filament::Filament, lattice::Lattice, move_vector::Vector{Int})
+
+    # Remove occupancy
+    for i in 1:filament.lf
+        pos = filament.coors[:, i]
+        delete!(lattice.occupancy, pos)
+    end
+    for i in 1:filament.lf
+        pos = filament.coors[:, i]
+        pos += move_vector
+        wrap_pos!(lattice, pos)
+        if pos in keys(lattice.occupancy)
+            return false
+        end
+        lattice.occupancy[copy(pos)] = (filament.index, i)
+    end
+    return true
+end
+
+# Should I be accepting the current or just propose again?
+function attempt_translation_move!(system::System, lattice::Lattice)
     use_trial_coors!(system, lattice)
-    filaments_on_boundary = find_filaments_on_boundary(system, lattice)
-    if !translate_filaments_on_boundary!(filaments_on_boundary, lattice, dir)
-        system_accept_current!(system, lattice)
+    filament = select_rand_filament(system)
+    move_vector = generate_random_vector()
+    if !translate_filament!(filament, lattice, move_vector)
+        accept_current!(system, lattice)
         use_current_coors!(system, lattice)
-        println(check_filaments_contiguous(system, lattice))
+        return nothing
+    end
+    if !ring_and_system_connected(system, lattice, filament)
+        accept_current!(filament, lattice)
+        use_current_coors!(system, lattice)
+        return nothing
+    end
+    delta_energy = calc_energy_diff!(system, lattice, filament)
+    if accept_move(delta_energy)
+        accept_trial!(filament, lattice)
+    else
+        accept_current!(filament, lattice)
+    end
+    use_current_coors!(system, lattice)
+    return nothing
+end
+
+function find_split_points(filaments::Vector{Filament}, lattice::Lattice, dir::Int)
+    split_points::Vector{Int} = []
+    for filament in filaments
+        push!(split_points, 0)
+        if filament.coors[2, 1] == 0 && dir == -1
+            break
+        end
+        for i in 1:filament.lf
+            split_points[end] += 1
+            if filament.coors[2, i] == lattice.height
+                break
+            end
+        end
+    end
+    return split_points
+end
+
+function translate_filaments_with_split_points!(filaments::Vector{Filament},
+    split_points::Vector{Int}, lattice::Lattice, dir::Int)
+
+    # Remove lattice occupancy
+    for (filament, split_point) in zip(filaments, split_points)
+        for i in 1:split_point
+            pos = filament.coors[:, i]
+            delete!(lattice.occupancy, pos)
+        end
+    end
+
+    # Shift segments of filaments below split point only
+    for (filament, split_point) in zip(filaments, split_points)
+        for i in 1:split_point
+            filament.coors[2, i] += dir
+            pos = filament.coors[:, i]
+            if dir == -1
+                wrap_pos!(lattice, pos)
+            end
+            if !(pos in keys(lattice.occupancy))
+                lattice.occupancy[pos] = (filament.index, i)
+            else
+                return false
+            end
+        end
+    end
+    return true
+end
+
+function attempt_radius_move!(system::System, lattice::Lattice)
+    dir = rand([-1, 1])
+    use_trial_coors!(system, lattice)
+    filaments = rand(system.filaments, rand(1:system.parms.Nfil))
+    split_points = find_split_points(filaments, lattice, dir)
+    if !translate_filaments_with_split_points!(filaments, split_points, lattice, dir)
+        accept_current!(system, lattice)
+        use_current_coors!(system, lattice)
         return nothing
     end
     update_radius(system, lattice, lattice.height + dir)
-    println(dir)
+    if !filaments_contiguous(system, lattice)
+        update_radius(system, lattice, lattice.height - dir)
+        accept_current!(system, lattice)
+        use_current_coors!(system, lattice)
+        return nothing
+    end
     if dir == 1
         # Should I be accepting the current or just propose again?
         if !ring_and_system_connected(system, lattice, system.filaments[1])
             update_radius(system, lattice, lattice.height - dir)
-            system_accept_current!(system, lattice)
+            accept_current!(system, lattice)
             use_current_coors!(system, lattice)
-            println(check_filaments_contiguous(system, lattice))
             return nothing
         end
     end
-    delta_energy = calc_system_energy_diff!(system::System, lattice::Lattice)
-    println(delta_energy)
+    delta_energy = calc_energy_diff!(system::System, lattice::Lattice)
     if accept_move(delta_energy)
-        system_accept_trial!(system, lattice)
+        accept_trial!(system, lattice)
         use_current_coors!(system, lattice)
-        println(check_filaments_contiguous(system, lattice))
     else
         update_radius(system, lattice, lattice.height - dir)
-        system_accept_current!(system, lattice)
+        accept_current!(system, lattice)
         use_current_coors!(system, lattice)
-        println(check_filaments_contiguous(system, lattice))
     end
     return nothing
 end
 
-function write_ops()
+function select_move(simparms::SimulationParams)
+    if simparms.radius_move_freq > rand(Float64)
+        return attempt_radius_move!
+    else
+        return attempt_translation_move!
+    end
+end
+
+function prepare_op_file(filename::String)
+    op_file = open(filename, "w")
+    header = "step energy height radius"
+    println(op_file, header)
+    return op_file
+end
+
+function write_ops(system::System, lattice::Lattice, step::Int, op_file::IOStream)
+    println(op_file, "$step $(system.energy) $(lattice.height) $(system.radius)")
 end
 
 # filebase or already setup files
-function run(system::System, lattice::Lattice, filebase, simparms)
-    for _ in 1:simparms.steps
+function run(system::System, lattice::Lattice, simparms::SimulationParams)
+    op_file = prepare_op_file(simparms.op_filename)
+    for step in 1:simparms.steps
+        println(step)
         attempt_move! = select_move(simparms)
         attempt_move!(system, lattice)
-        if step % simparms.write_interval
-            write_ops()
+        if step % simparms.write_interval == 0
+            system.energy = calc_total_energy(system, lattice)
+            write_ops(system, lattice, step, op_file)
         end
     end
+    close(op_file)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,12 +8,25 @@ T = 300
 delta = 36e-9
 Xc = 12e-9
 EI = 3.6e-26
-Lf = 2.988e-6
-lf = 4
+Lf = 3e-6
 Nfil = 6
 Nsca = 4
 
-lattice = armc.Lattice(Nsca*lf - Nsca - 1)
+steps = 1e4
+write_interval = 1e2
+radius_move_freq = 0.5
+op_filename = "test.ops"
+
+lf = armc.calc_lf(Lf, delta)
+Lf = armc.calc_Lf(lf, delta)
+lattice_height = armc.calc_lattice_height(Nsca, lf)
+max_radius = Lf*Nsca
+
+lattice = armc.Lattice(lattice_height)
 filaments = armc.generate_starting_config!(lattice, Nfil, Nsca, lf)
 sysparms = armc.SystemParams(kd, ks, T, delta, Xc, EI, Lf, lf, Nfil, Nsca)
-system = armc.System(filaments, 2.988e-6*Nsca, sysparms)
+simparms = armc.SimulationParams(steps, write_interval, radius_move_freq, op_filename)
+system = armc.System(sysparms, filaments, max_radius)
+op_file = armc.prepare_op_file(op_filename)
+armc.run(system, lattice, op_file, simparms)
+close(op_file)
