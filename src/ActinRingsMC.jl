54994c0c315847cdbf41665eb15a1337d2f2eac8
Alexander <alexandercumberworth@gmail.com>
2021-11-19 15:50:05 +0100

    Add back multiplier for lattice height

--- src/ActinRingsMC.jl
+++ src/ActinRingsMC.jl
@@ -759,9 +759,9 @@ function attempt_radius_move!(system::System, lattice::Lattice, biases::Biases)
     end
 
     delta_energy = energy_diff(system, lattice, biases)
-    #mult = lattice.trial_height / lattice.current_height
-    #accept = accept_move(system, delta_energy, mult)
-    accept = accept_move(system, delta_energy)
+    mult = lattice.trial_height / lattice.current_height
+    accept = accept_move(system, delta_energy, mult)
+    #accept = accept_move(system, delta_energy)
     if accept
         accept_trial!(system, lattice)
     else
